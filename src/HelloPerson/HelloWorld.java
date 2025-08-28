7a5efc198911119ffb51b9411c831321cb42f662
Thomas Naughton <naughtont@ornl.gov>
2025-08-27 23:25:10 -0400

    hello world + person (v1)
    Signed-off-by: Thomas Naughton <naughtont@ornl.gov>

---
+++ src/HelloPerson/HelloWorld.java
@@ -0,0 +1,36 @@
+/**
+ * Short example for creating an instance of a Person class
+ * and printing some output.
+ *
+ * @version v1
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Instantiate Person using the constructor
+        Person myBuddy = new Person("Tommy", 21);
+        Person mySister = new Person("Catherine", 16);
+
+        // Call methods
+        myBuddy.greet(mySister.getName());
+        mySister.greet(myBuddy.getName());
+
+        System.out.println(String.format("%10s is %d",
+                                         myBuddy.getName(),
+                                         myBuddy.getAge()));
+
+        System.out.println(String.format("%10s is %d",
+                                         mySister.getName(),
+                                         mySister.getAge()));
+
+        myBuddy.addToAge();
+
+        System.out.println(String.format("HAPPY BIRTHDAY %s!",
+                                         myBuddy.getName()));
+
+        System.out.println(String.format("%10s is %d",
+                                         myBuddy.getName(),
+                                         myBuddy.getAge()));
+    }
+}
+
